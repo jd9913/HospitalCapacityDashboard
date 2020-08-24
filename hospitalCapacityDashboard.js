32979fdd7370333c29e4d48177b77bae7869ec37
Jennifer <67403842+jd9913@users.noreply.github.com>
2020-08-24 16:32:47 -0700

    create the containers for the graphs and the beginning js

--- hospitalCapacityDashboard.js
+++ hospitalCapacityDashboard.js
@@ -1,16 +1,26 @@
 
 
 
-var covidEDEl = document.getElementById('covidED');
+
 var covidAdmitEl = document.getElementById('covidAdmit');
 var covidInptEl = document.getElementById('covidInpt');
 var lineDataEl = document.getElementById('lineData');
 
 
-var covidED = new Chart(covidEDEl, {
-    type: 'pie',
-    data: {
-        labels: 
-    }
-})
-
+var covidEDEl = new Chart(document.getElementById("covidEDGraph"), {
+      type: 'pie',
+        data: {
+            labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+            datasets: [{
+                label: "Population (millions)",
+                backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
+                data: [2478, 5267, 734, 784, 433]
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Predicted world population (millions) in 2050'
+            }
+        }
+    });
