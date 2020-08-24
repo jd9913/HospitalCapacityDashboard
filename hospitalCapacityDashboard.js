a5f54a5541dc41d80d5d0405e614e5a12f76124d
Jennifer <67403842+jd9913@users.noreply.github.com>
2020-08-24 15:37:00 -0700

    file location change and .gitignore

---
+++ hospitalCapacityDashboard.js
@@ -0,0 +1,16 @@
+
+
+
+var covidEDEl = document.getElementById('covidED');
+var covidAdmitEl = document.getElementById('covidAdmit');
+var covidInptEl = document.getElementById('covidInpt');
+var lineDataEl = document.getElementById('lineData');
+
+
+var covidED = new Chart(covidEDEl, {
+    type: 'pie',
+    data: {
+        labels: 
+    }
+})
+
